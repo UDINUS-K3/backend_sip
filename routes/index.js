cdb1441802d1039bca0fd01969e2a7a07929b753
wisnuaga <wisnu.aga@gmail.com>
2020-04-14 20:25:50 +0700

    update source

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,37 @@ const information = require('./information');
 const comment = require('./comment');
 const auth = require('./auth');
 const user = require('./user');
+const upload = require('../app/middlewares/upload-image').upload;
+const cloudinary = require('../app/services/cloudinary');
+const fs = require('file-system');
 
 module.exports = (app) => {
     app.get('/', function (req, res, next) {
         return res.status(200).json({
             message: "hello dev!"
         });
+    });
+
+    app.post('/upload_images', upload, async (req, res) => {
+        try {
+            const uploader = async (path) => await cloudinary.uploads(path, 'images');
+
+            const file = req.file
+            const { path } = file
+            const newPath = await uploader(path)
+            fs.unlinkSync(path)
+
+            res.status(200).json({
+                message: 'success',
+                data: newPath
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({
+                message: 'failed',
+                error: error.message
+            })
+        }
     });
 
     // app routes
