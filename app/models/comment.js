1d5b12c502ce02f6d6f565b2cdb566962119ea04
wisnu <wisnu.aga@gmail.com>
2020-06-01 23:52:53 +0700

    remove token temporary

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,41 +1,45 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false
+  const Comment = sequelize.define(
+    "Comment",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      comment: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+      },
+      information_id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+        field: "deleted_at",
+      },
     },
-    comment: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    user_id: {
-      allowNull: false,
-      type: DataTypes.UUID
-    },
-    information_id: {
-      allowNull: false,
-      type: DataTypes.UUID
-    },
-    deleted_at: {
-      type: DataTypes.DATE,
-      field: 'deleted_at'
+    {
+      timestamps: true,
+      underscored: true,
+      paranoid: true,
+      tableName: "comments",
     }
-  }, {
-    timestamps: true,
-    underscored: true,
-    paranoid: true,
-    tableName: 'comments'
-  });
+  );
   Comment.associate = function (models) {
     // associations can be defined here
     Comment.belongsTo(models.Information, {
-      foreignKey: 'information_id'
+      foreignKey: "information_id",
     });
     Comment.belongsTo(models.User, {
-      foreignKey: 'user_id'
+      foreignKey: "user_id",
     });
   };
   return Comment;
